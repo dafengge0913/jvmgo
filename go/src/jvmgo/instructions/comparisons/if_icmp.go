8b05b59035bd631d2b87f1e050e4f70477dd0d83
dafengge0913 <dafengge0913@hotmail.com>
2018-04-11 11:49:48 +0800

    Instruction comparisons

---
+++ go/src/jvmgo/instructions/comparisons/if_icmp.go
@@ -0,0 +1,73 @@
+package comparisons
+
+import (
+	"jvmgo/instructions/base"
+	"jvmgo/rtda"
+)
+
+func _icmpPop(frame *rtda.Frame) (val1, val2 int32) {
+	stack := frame.OperandStack()
+	val2 = stack.PopInt()
+	val1 = stack.PopInt()
+	return
+}
+
+type IF_ICMPEQ struct {
+	base.BranchInstruction
+}
+
+func (ins *IF_ICMPEQ) Execute(frame *rtda.Frame) {
+	if val1, val2 := _icmpPop(frame); val1 == val2 {
+		base.Branch(frame, ins.Offset)
+	}
+}
+
+type IF_ICMPNE struct {
+	base.BranchInstruction
+}
+
+func (ins *IF_ICMPNE) Execute(frame *rtda.Frame) {
+	if val1, val2 := _icmpPop(frame); val1 != val2 {
+		base.Branch(frame, ins.Offset)
+	}
+}
+
+type IF_ICMPLT struct {
+	base.BranchInstruction
+}
+
+func (ins *IF_ICMPLT) Execute(frame *rtda.Frame) {
+	if val1, val2 := _icmpPop(frame); val1 < val2 {
+		base.Branch(frame, ins.Offset)
+	}
+}
+
+type IF_ICMPLE struct {
+	base.BranchInstruction
+}
+
+func (ins *IF_ICMPLE) Execute(frame *rtda.Frame) {
+	if val1, val2 := _icmpPop(frame); val1 <= val2 {
+		base.Branch(frame, ins.Offset)
+	}
+}
+
+type IF_ICMPGT struct {
+	base.BranchInstruction
+}
+
+func (ins *IF_ICMPGT) Execute(frame *rtda.Frame) {
+	if val1, val2 := _icmpPop(frame); val1 > val2 {
+		base.Branch(frame, ins.Offset)
+	}
+}
+
+type IF_ICMPGE struct {
+	base.BranchInstruction
+}
+
+func (ins *IF_ICMPGE) Execute(frame *rtda.Frame) {
+	if val1, val2 := _icmpPop(frame); val1 >= val2 {
+		base.Branch(frame, ins.Offset)
+	}
+}
