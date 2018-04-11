43dc9c67c7d08e7f750faf9ec5ff38664a9fc9f7
dafengge0913 <dafengge0913@hotmail.com>
2018-04-11 12:20:09 +0800

    Add comments

--- go/src/jvmgo/classfile/member_info.go
+++ go/src/jvmgo/classfile/member_info.go
@@ -44,6 +44,14 @@ func (mi *MemberInfo) Name() string {
 	return mi.cp.getUtf8(mi.nameIndex)
 }
 
+
+// (1) 基本类型byte short char int long float和double的描述符是单个字母
+//     分别对应B S C I J F和D 注意 long的描述符是J而不是L
+// (2) 引用类型的描述符是L＋类的完全限定名＋分号
+// (3) 数组类型的描述符是[＋数组元素类型描述符
+// (4) 字段描述符就是字段类型的描述符
+// (5) 方法描述符是 参数类型的域描述符按照申明顺序放入一对括号中后+返回值类型描述符
+//     其中void返回值由单个字母V表示。
 func (mi *MemberInfo) Descriptor() string {
 	return mi.cp.getUtf8(mi.descriptorIndex)
 }
