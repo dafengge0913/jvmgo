ecbc178afc5da00812e980ab3a219840a56252b2
dafengge0913 <dafengge0913@hotmail.com>
2018-04-19 16:54:27 +0800

    Object hashCode and clone

--- go/src/jvmgo/native/java/lang/Object.go
+++ go/src/jvmgo/native/java/lang/Object.go
@@ -3,10 +3,13 @@ package lang
 import (
 	"jvmgo/native"
 	"jvmgo/rtda"
+	"unsafe"
 )
 
 func init() {
 	native.Register("java/lang/Object", "getClass", "()Ljava/lang/Class;", getClass)
+	native.Register("java/lang/Object", "hashCode", "()I", hashCode)
+	native.Register("java/lang/Object", "clone", "()Ljava/lang/Object;", clone)
 }
 
 // public final native Class<?> getClass();
@@ -14,4 +17,22 @@ func getClass(frame *rtda.Frame) {
 	this := frame.LocalVars().GetThis()
 	class := this.Class().JClass()
 	frame.OperandStack().PushRef(class)
+}
+
+// public native int hashCode();
+func hashCode(frame *rtda.Frame) {
+	this := frame.LocalVars().GetThis()
+	hash := int32(uintptr(unsafe.Pointer(this)))
+	frame.OperandStack().PushInt(hash)
+}
+
+
+// protected native Object clone() throws CloneNotSupportedException;
+func clone(frame *rtda.Frame) {
+	this := frame.LocalVars().GetThis()
+	cloneable := this.Class().Loader().LoadClass("java/lang/Cloneable")
+	if !this.Class().IsImplements(cloneable) {
+		panic("java.lang.CloneNotSupportedException")
+	}
+	frame.OperandStack().PushRef(this.Clone())
 }
